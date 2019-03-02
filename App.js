c22d138586bfb7a91e27cdaf18a6a2ceb35eb710
kien1702 <41613322+kien1702@users.noreply.github.com>
2019-03-02 23:43:58 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import button from './components/button.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+<div className="calculator">
+  <div className="row"
+    <button>8</button>
+      <button>9</button>
+        <button>0</button>
+    </div>
+     </div>
+      </div>
+    );
+  }
+}
+
+export default App;
